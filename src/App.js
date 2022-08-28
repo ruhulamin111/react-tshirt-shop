1318b2250c3686a296a7e0e7ac31dff06eb5d467
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-28 09:09:11 +0600

    active nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Header from './components/Header/Header';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home';
+import Orders from './components/Orders/Orders';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/orders' element={<Orders></Orders>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './Header.css';
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+    const navActive = ({ isActive }) => {
+        return {
+            textDecoration: isActive ? 'underline' : '',
+            color: isActive ? 'red' : '',
+        }
+    }
+
     return (
         <div>
+            <nav>
+                <NavLink style={navActive} to='/home'>Home</NavLink>
+                <NavLink style={navActive} to='/orders'>Orders</NavLink>
+            </nav>
 
         </div>
     );
